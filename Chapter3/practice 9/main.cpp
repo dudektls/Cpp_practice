b570df526684e3808400cdcfae01afcd46cc7e58
컴파일에러 <parkyw.csw@gmail.com>
2026-04-19 01:43:11 +0900

    Create main.cpp
    chaper 2의 13번 문제를 클래스를 이용하여 다시작성. 
    (처음작성하는거라 해설참조했음. 다시 해봐야 할듯)

---
+++ Chapter3/practice 9/main.cpp
@@ -0,0 +1,10 @@
+#include "head.h"
+#include <string>
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "에스프레소 3000원, 아메리카노 3500원, 카푸치노 4000원입니다." << endl;
+	Caffe myCaffe;
+	myCaffe.run();
+}
