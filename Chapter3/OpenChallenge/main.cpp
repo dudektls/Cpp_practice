f62dd9caea78ccf70c3a05965776985bf9a8c1d2
컴파일에러 <parkyw.csw@gmail.com>
2026-04-18 19:34:23 +0900

    Create main.cpp
    오픈챌린지 메인함수 부분

---
+++ Chapter3/OpenChallenge/main.cpp
@@ -0,0 +1,21 @@
+#include "head.h"
+#include <string>
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	Exp a(3, 2);
+	Exp b(9);
+	Exp c;
+
+	cout << a.getValue() << ' ' << b.getValue() << ' ' << c.getValue() << endl;
+	cout <<"a의 베이스 :" << a.getbase() << ' , ' <<"지수 :" << a.getExp() << endl;
+
+	if (a.equals(b))
+		cout << "same" << endl;
+	else
+		cout << "not same" << endl;
+
+	return 0;
+}
