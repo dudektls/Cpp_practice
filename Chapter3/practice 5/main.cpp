543c6e865eeac86934f982576d1b1d348fb3febf
컴파일에러 <parkyw.csw@gmail.com>
2026-04-18 22:05:44 +0900

    Create main.cpp
    간단한 커피머신 로직. main()과 그의 결과를 보고 CoffeeMachine 클래스 작성

---
+++ Chapter3/practice 5/main.cpp
@@ -0,0 +1,16 @@
+#include "head.h"
+#include <string>
+#include<iostream>
+using namespace std;
+
+int main() {
+	CoffeeMachine java(5, 10, 6);
+	java.drinkEspresso();
+	java.show();
+	java.drinkAmericano();
+	java.show();
+	java.drinkSugarCoffee();
+	java.show();
+	java.fill();
+	java.show();
+}
