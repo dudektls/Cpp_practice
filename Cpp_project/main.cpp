6b17f82f13c9040eb2aef0ed567308789428d828
컴파일에러 <parkyw.csw@gmail.com>
2026-05-27 10:47:22 +0900

    Create main.cpp

---
+++ Cpp_project/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "pet.h"
+
+using namespace std;
+
+int main() {
+	// 시작
+	// 게임 시작 멘트 출력
+	// 이름과 종류를 입력
+	// 키울 기간을 입력
+	// 메인 게임 시작. 초기 스텟창 출력
+	// 먹이 선택 & 활동 선택중 택일 하나 고르면 그 하위 선택지 출력및 입력
+	// 최종 결말 출력
+}
