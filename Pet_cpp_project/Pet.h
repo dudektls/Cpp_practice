0b70b2c5c8baca042c449de9a5f4bf0155e41092
컴파일에러 <parkyw.csw@gmail.com>
2026-06-06 21:10:53 +0900

    Create Pet.h

---
+++ Pet_cpp_project/Pet.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include "Food.h"
+#include "Act.h"
+using namespace std;
+
+class Pet {
+private:
+	string petType;
+protected:
+	int Energy = 70;
+	int Love = 0;
+	int MaxEat = 3;
+	int MaxAct = 2;
+
+public:
+	string petName;
+	int RunTurn = 0;
+	
+	Pet(string name,int Turn) {  //디폴트 매개변수 생성자
+		petName = name;
+		RunTurn = Turn;
+	}
+	virtual void eat(string type) {};  //먹이 먹는 메서드
+	virtual void act(string type) {};  // 활동하는 메서드
+
+	virtual void printStatus() {};   // 활동하면서 실시간으로 변화할 스탯창 
+	virtual void printStatus(int i) {}; // 하루 마무리하면서 출력할 최후 스탯창
+};
