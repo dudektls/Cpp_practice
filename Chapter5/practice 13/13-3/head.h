9bc4e9bf9f7efa5c2acde7d8e856d0b68db0f717
컴파일에러 <parkyw.csw@gmail.com>
2026-06-12 19:02:51 +0900

    Create head.h

---
+++ Chapter5/practice 13/13-3/head.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class collector {
+	int* p;
+	int size = 0;
+public:
+	collector(int size, int value[]);
+	void show();
+	int getsize() { return size; }
+	int get(int index) { return p[index]; }
+
+	~collector() {
+		delete[] p;
+	}
+
+	collector(const collector& src);
+};
