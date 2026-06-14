ba60891244abff4c2f6ba1dde2f6173e50955d6f
컴파일에러 <parkyw.csw@gmail.com>
2026-06-14 18:11:55 +0900

    Create main.cpp

---
+++ Chapter7/Tutorial7-05/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+#include "head.h"
+using namespace std;
+
+int main() {
+	power a(3, 5), b(3, 5);
+	a.show();
+	b.show();
+	if (a == b) {
+		cout << "Equal" << endl;
+	}
+	else
+		cout << "Not Equal" << endl;
+	return 0;
+}
