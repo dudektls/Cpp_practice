53f9f788fae0b98f880e01c39ce4284f4e15a53f
컴파일에러 <parkyw.csw@gmail.com>
2026-06-15 01:16:48 +0900

    Create main.cpp

---
+++ Chapter8/practice 01/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+#include "head.h"
+#include"NamedCircle.h"
+using namespace std;
+
+int main() {
+
+	NamedCircle waffle(3, "waffle");
+	waffle.show();
+
+	return 0;
+}
