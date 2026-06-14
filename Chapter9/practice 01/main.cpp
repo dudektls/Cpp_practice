949c0bdf78320dc376b1a8f5241252d6e2dc9d2b
컴파일에러 <parkyw.csw@gmail.com>
2026-06-15 04:04:49 +0900

    Create main.cpp
    converter를 상속받아 원화를 달러로 바꾸는 Won2Dollor클래스를 작성

---
+++ Chapter9/practice 01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<string>
+#include "head.h"
+#include "head2.h"
+using namespace std;
+
+int main() {
+	Won2Dollar w2d("원", "달러", 1450);  // 1달러에 1450원
+	w2d.run();
+	return 0;
+}
