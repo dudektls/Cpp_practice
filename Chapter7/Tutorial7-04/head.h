5a084469681f544b945bdd7ce0135a60d4c749f4
컴파일에러 <parkyw.csw@gmail.com>
2026-06-14 18:08:38 +0900

    Create head.h

---
+++ Chapter7/Tutorial7-04/head.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include<string>
+using namespace std;
+
+class power {
+	int kick;
+	int punch;
+public:
+	power(int kick = 0, int punch = 0) {
+		this->kick = kick;
+		this->punch = punch;
+	}
+	void show();
+	power operator+(power op2);
+
+};
